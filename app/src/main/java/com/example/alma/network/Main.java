d40c2fd43a0d17f1e79fb7b868ac2bff4ee69ba9
JamMaster <wawrzynchonewicz@gmail.com>
2017-06-14 11:51:24 +0200

    Created a layout for the main activity

--- app/src/main/java/com/example/alma/network/Main.java
+++ app/src/main/java/com/example/alma/network/Main.java
@@ -2,26 +2,16 @@ package com.example.alma.network;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Main extends Activity implements OnClickListener {
+public class Main extends Activity{
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        findViewById(R.id.my_button).setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View arg0) {
-        Button b = (Button) findViewById(R.id.my_button);
-
-        b.setClickable(false);
-        new LongRunningGetIO(this).execute();
+        setContentView(R.layout.activity_main);
     }
 }
-
-
