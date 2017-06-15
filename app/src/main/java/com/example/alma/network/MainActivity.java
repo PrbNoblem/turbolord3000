cf26dac3d22d69f817de36dd39cba1b7151d6a63
JamMaster <wawrzynchonewicz@gmail.com>
2017-06-15 15:40:40 +0200

    Started fresh to avoid weird errors

--- app/src/main/java/com/example/alma/network/MainActivity.java
+++ app/src/main/java/com/example/alma/network/MainActivity.java
@@ -1,35 +1,13 @@
 package com.example.alma.network;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
-public class MainActivity extends Activity {
-    public static final String EXTRA_MESSAGE = "com.example.alma.network.EXTRA_MESSAGE";
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setupSeekBar();
-    }
-
-    private void setupSeekBar() {
-        SeekBar seekBar = (SeekBar) findViewById(R.id.seekbar);
-        final TextView textView = (TextView) findViewById(R.id.seek_state);
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                textView.setText(Integer.toHexString(i).toUpperCase());
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
     }
 }
